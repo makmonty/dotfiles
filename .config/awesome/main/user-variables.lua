0ed620db726da9f3271070e6269846aa5bb93ebd
Ángel Brasero Moreno <angel.brasero@gmail.com>
2021-11-26 14:53:27 +0100

    Commented YouCompleteMe vim plugin. Split AwesomeWM config in parts. Added some Rofi themes

---
+++ .config/awesome/main/user-variables.lua
@@ -0,0 +1,45 @@
+local awful = require("awful")
+
+-- Themes define colours, icons, font and wallpapers.
+local theme = "/home/angel/.config/awesome/theme.lua"
+
+-- This is used later as the default terminal and editor to run.
+local terminal = "xfce4-terminal"
+local editor = "gvim" -- os.getenv("EDITOR") or "nano"
+local editor_cmd = terminal .. " -e " .. editor
+
+-- Default modkey.
+-- Usually, Mod4 is the key with a logo between Control and Alt.
+-- If you do not like this or do not have such a key,
+-- I suggest you to remap Mod4 to another key using xmodmap or other tools.
+-- However, you can use another modifier like Mod1, but it may interact with others.
+local modkey = "Mod4"
+
+-- Table of layouts to cover with awful.layout.inc, order matters.
+local layouts = {
+    awful.layout.suit.floating,
+    awful.layout.suit.tile,
+    awful.layout.suit.tile.left,
+    awful.layout.suit.tile.bottom,
+    awful.layout.suit.tile.top,
+    awful.layout.suit.fair,
+    awful.layout.suit.fair.horizontal,
+    awful.layout.suit.spiral,
+    awful.layout.suit.spiral.dwindle,
+    awful.layout.suit.max,
+    awful.layout.suit.max.fullscreen,
+    awful.layout.suit.magnifier,
+    awful.layout.suit.corner.nw,
+    -- awful.layout.suit.corner.ne,
+    -- awful.layout.suit.corner.sw,
+    -- awful.layout.suit.corner.se,
+}
+
+return {
+    theme = theme,
+    terminal = terminal,
+    editor = editor,
+    editor_cmd = editor_cmd,
+    modkey = modkey,
+    layouts = layouts
+}
