8e4f184c3a1d06fec59dbebadf10bba5ada40580
Ángel Brasero Moreno <angel.brasero@gmail.com>
2021-11-30 20:22:09 +0100

    Added window decorations

--- .config/awesome/theme.lua
+++ .config/awesome/theme.lua
@@ -8,13 +8,24 @@ local dpi = xresources.apply_dpi
 
 local gfs = require("gears.filesystem")
 local themes_path = gfs.get_themes_dir()
+local shapes = require("helpers.shapes")
+
+local function titlebar_button(color)
+    return shapes.circle_filled(color, 128, 32)
+end
+
+local titlebar_close_button = titlebar_button("#E01A4F")
+local titlebar_maximize_button = titlebar_button("#F9C22E")
+local titlebar_minimize_button = titlebar_button("#9CD08F")
+local titlebar_extra_button_active = titlebar_button("#9CD08F")
+local titlebar_extra_button_inactive = titlebar_button("#F15946")
 
 local theme = {}
 
 theme.font          = "sans 8"
 
 theme.bg_normal     = "#222222"
-theme.bg_focus      = "#535d6c"
+theme.bg_focus      = "#222222" --"#535d6c"
 theme.bg_urgent     = "#ff0000"
 theme.bg_minimize   = "#444444"
 theme.bg_systray    = theme.bg_normal
@@ -71,33 +82,33 @@ theme.menu_width  = dpi(100)
 --theme.bg_widget = "#cc0000"
 
 -- Define the image to load
-theme.titlebar_close_button_normal = themes_path.."default/titlebar/close_normal.png"
-theme.titlebar_close_button_focus  = themes_path.."default/titlebar/close_focus.png"
+theme.titlebar_close_button_normal = titlebar_close_button --themes_path.."default/titlebar/close_normal.png"
+theme.titlebar_close_button_focus  = titlebar_close_button --themes_path.."default/titlebar/close_focus.png"
 
-theme.titlebar_minimize_button_normal = themes_path.."default/titlebar/minimize_normal.png"
-theme.titlebar_minimize_button_focus  = themes_path.."default/titlebar/minimize_focus.png"
+theme.titlebar_minimize_button_normal = titlebar_minimize_button --themes_path.."default/titlebar/minimize_normal.png"
+theme.titlebar_minimize_button_focus  = titlebar_minimize_button --themes_path.."default/titlebar/minimize_focus.png"
 
-theme.titlebar_ontop_button_normal_inactive = themes_path.."default/titlebar/ontop_normal_inactive.png"
-theme.titlebar_ontop_button_focus_inactive  = themes_path.."default/titlebar/ontop_focus_inactive.png"
-theme.titlebar_ontop_button_normal_active = themes_path.."default/titlebar/ontop_normal_active.png"
-theme.titlebar_ontop_button_focus_active  = themes_path.."default/titlebar/ontop_focus_active.png"
+theme.titlebar_maximized_button_normal_inactive = titlebar_maximize_button --themes_path.."default/titlebar/maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_inactive  = titlebar_maximize_button --themes_path.."default/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_active = titlebar_maximize_button --themes_path.."default/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_active  = titlebar_maximize_button --themes_path.."default/titlebar/maximized_focus_active.png"
 
-theme.titlebar_sticky_button_normal_inactive = themes_path.."default/titlebar/sticky_normal_inactive.png"
-theme.titlebar_sticky_button_focus_inactive  = themes_path.."default/titlebar/sticky_focus_inactive.png"
-theme.titlebar_sticky_button_normal_active = themes_path.."default/titlebar/sticky_normal_active.png"
-theme.titlebar_sticky_button_focus_active  = themes_path.."default/titlebar/sticky_focus_active.png"
+theme.titlebar_ontop_button_normal_inactive = titlebar_extra_button_inactive --themes_path.."default/titlebar/ontop_normal_inactive.png"
+theme.titlebar_ontop_button_focus_inactive  = titlebar_extra_button_inactive --themes_path.."default/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_active = titlebar_extra_button_active --themes_path.."default/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_active  = titlebar_extra_button_active --themes_path.."default/titlebar/ontop_focus_active.png"
 
-theme.titlebar_floating_button_normal_inactive = themes_path.."default/titlebar/floating_normal_inactive.png"
-theme.titlebar_floating_button_focus_inactive  = themes_path.."default/titlebar/floating_focus_inactive.png"
-theme.titlebar_floating_button_normal_active = themes_path.."default/titlebar/floating_normal_active.png"
-theme.titlebar_floating_button_focus_active  = themes_path.."default/titlebar/floating_focus_active.png"
+theme.titlebar_sticky_button_normal_inactive = titlebar_extra_button_inactive --themes_path.."default/titlebar/sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_inactive  = titlebar_extra_button_inactive --themes_path.."default/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_active = titlebar_extra_button_active --themes_path.."default/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_active  = titlebar_extra_button_active --themes_path.."default/titlebar/sticky_focus_active.png"
 
-theme.titlebar_maximized_button_normal_inactive = themes_path.."default/titlebar/maximized_normal_inactive.png"
-theme.titlebar_maximized_button_focus_inactive  = themes_path.."default/titlebar/maximized_focus_inactive.png"
-theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/maximized_normal_active.png"
-theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
+theme.titlebar_floating_button_normal_inactive = titlebar_extra_button_inactive --themes_path.."default/titlebar/floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_inactive  = titlebar_extra_button_inactive --themes_path.."default/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_active = titlebar_extra_button_active --themes_path.."default/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_active  = titlebar_extra_button_active --themes_path.."default/titlebar/floating_focus_active.png"
 
-theme.wallpaper = "/home/angel/Imágenes/Fondos/Gaming/pixel nintendo.jpg"
+theme.wallpaper = "/home/angel/Imágenes/Fondos/Retro_Love_by_swiebel.jpg"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
