270e06aaf53298587e303a7619713963574ecb12
Ángel Brasero Moreno <angel.brasero@gmail.com>
2022-05-16 18:01:57 +0200

    Migrated Neovim from Plug to Packer

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,68 +1,65 @@
-local Plug = require('helpers.plug')
+local plugins = require('packer').startup(function(use)
+	-- Color scheme
+	use 'srcery-colors/srcery-vim'
+	-- Filesystem tree
+	use 'preservim/nerdtree'
+	-- Icons for NERDTree
+	use 'ryanoasis/vim-devicons'
+	-- Git integration for NERDTree
+	use 'Xuyuanp/nerdtree-git-plugin'
+	-- Colorize NERDTree
+	use 'tiagofumo/vim-nerdtree-syntax-highlight'
+	-- Treesitter for syntax highlighting
+	use {'nvim-treesitter/nvim-treesitter', { run = vim.fn[':TSUpdate'] }}
+	-- Javascript integration
+	use 'pangloss/vim-javascript'
+	-- Vue syntax highlight
+	use 'posva/vim-vue'
+	-- Git plugin
+	use 'tpope/vim-fugitive'
+	-- Suggestions and completion
+	use {'neoclide/coc.nvim', { branch = 'release' }}
+	-- Git marks in the gutter
+	use 'airblade/vim-gitgutter'
+	-- Statusbar
+	use 'vim-airline/vim-airline'
+	-- Typescript integration
+	use 'leafgarland/typescript-vim'
+	-- Editorconfig integration
+	use 'editorconfig/editorconfig-vim'
+	-- Colorize parentheses
+	use 'kien/rainbow_parentheses.vim'
+	-- LSP marks in the gutter
+	use 'w0rp/ale'
+	-- Local vimrc
+	use 'embear/vim-localvimrc'
+	-- Fuzzy finder
+	use {'junegunn/fzf', { run = vim.fn['fzf#install'] }}
+	use 'junegunn/fzf.vim'
+	--use 'nvim-lua/plenary.nvim'
+	--use 'nvim-telescope/telescope.nvim'
+	-- Show css colors inline
+	use 'ap/vim-css-color'
+	-- Underlines the current word and its appearances
+	use 'itchyny/vim-cursorword'
+	-- Change easily the surrounding characters
+	use 'tpope/vim-surround'
+	-- For writers
+	use 'preservim/vim-pencil'
+	-- Make multiline changes
+	use 'mg979/vim-visual-multi'
+	-- Inline git blame
+	use 'APZelos/blamer.nvim'
+	-- Symbols and tags in a sidebar
+	use 'liuchengxu/vista.vim'
+	-- Comment easily
+	use 'preservim/nerdcommenter'
+	-- Handlebars integration
+	use 'mustache/vim-mustache-handlebars'
+	-- Search in the current window
+	use 'ggandor/leap.nvim'
 
-Plug.begin()
-
--- Color scheme
-Plug 'srcery-colors/srcery-vim'
--- Filesystem tree
-Plug 'preservim/nerdtree'
--- Icons for NERDTree
-Plug 'ryanoasis/vim-devicons'
--- Git integration for NERDTree
-Plug 'Xuyuanp/nerdtree-git-plugin'
--- Colorize NERDTree
-Plug 'tiagofumo/vim-nerdtree-syntax-highlight'
--- Treesitter for syntax highlighting
-Plug('nvim-treesitter/nvim-treesitter', { run = vim.fn[':TSUpdate'] })
--- Javascript integration
-Plug 'pangloss/vim-javascript'
--- Vue syntax highlight
-Plug 'posva/vim-vue'
--- Git plugin
-Plug 'tpope/vim-fugitive'
--- Suggestions and completion
-Plug('neoclide/coc.nvim', { branch = 'release' })
--- Git marks in the gutter
-Plug 'airblade/vim-gitgutter'
--- Statusbar
-Plug 'vim-airline/vim-airline'
--- Typescript integration
-Plug 'leafgarland/typescript-vim'
--- Editorconfig integration
-Plug 'editorconfig/editorconfig-vim'
--- Colorize parentheses
-Plug 'kien/rainbow_parentheses.vim'
--- LSP marks in the gutter
-Plug 'w0rp/ale'
--- Local vimrc
-Plug 'embear/vim-localvimrc'
--- Fuzzy finder
-Plug('junegunn/fzf', { run = vim.fn['fzf#install'] })
-Plug 'junegunn/fzf.vim'
---Plug 'nvim-lua/plenary.nvim'
---Plug 'nvim-telescope/telescope.nvim'
--- Show css colors inline
-Plug 'ap/vim-css-color'
--- Underlines the current word and its appearances
-Plug 'itchyny/vim-cursorword'
--- Change easily the surrounding characters
-Plug 'tpope/vim-surround'
--- For writers
-Plug 'preservim/vim-pencil'
--- Make multiline changes
-Plug 'mg979/vim-visual-multi'
--- Inline git blame
-Plug 'APZelos/blamer.nvim'
--- Symbols and tags in a sidebar
-Plug 'liuchengxu/vista.vim'
--- Comment easily
-Plug 'preservim/nerdcommenter'
--- Handlebars integration
-Plug 'mustache/vim-mustache-handlebars'
--- Search in the current window
-Plug 'ggandor/leap.nvim'
-
-Plug.ends()
+end)
 
 -- NERDTree
 vim.cmd('let NERDTreeShowHidden = 1')
@@ -86,3 +83,5 @@ vim.cmd('let g:coc_global_extensions = ['..
 
 -- Leap
 require('leap').set_default_keymaps()
+
+return plugins
