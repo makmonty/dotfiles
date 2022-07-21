6c1f0f27e141cff5efe1fabb7d1bf7191258d83f
Ángel Brasero Moreno <angel.brasero@gmail.com>
2022-07-21 15:25:15 +0200

    Fixed LSP configurations

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -16,47 +16,12 @@ local plugins = require('packer').startup(function(use)
   use {'nvim-treesitter/nvim-treesitter', { run = vim.fn[':TSUpdate'] }}
   -- LSP
   use {
-    "williamboman/nvim-lsp-installer",
+    'williamboman/nvim-lsp-installer',
     -- Completion
     {'ms-jpq/coq_nvim', { branch = 'coq' }},
     {'ms-jpq/coq.artifacts', { branch = 'artifacts' }},
     {'ms-jpq/coq.thirdparty', { branch = '3p' }},
-    {
-      "neovim/nvim-lspconfig",
-      config = function()
-        require("nvim-lsp-installer").setup {
-          automatic_installation = true,
-        }
-        local lspconfig = require("lspconfig")
-        local coq = require("coq")
-        -- Here go the server setups
-        lspconfig.sumneko_lua.setup(coq.lsp_ensure_capabilities({
-          settings = {
-            Lua = {
-              diagnostics = {
-                globals = { 'vim' }
-              }
-            }
-          }
-        }))
-        local eslint_config = require("lspconfig.server_configurations.eslint")
-        lspconfig.eslint.setup(coq.lsp_ensure_capabilities({
-          cmd = { "yarn", "exec", unpack(eslint_config.default_config.cmd) }
-        }))
-        lspconfig.volar.setup(coq.lsp_ensure_capabilities({
-          filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json'}
-        }))
-        lspconfig.ember.setup(coq.lsp_ensure_capabilities({}))
-
-        local capabilities = vim.lsp.protocol.make_client_capabilities()
-        capabilities.textDocument.completion.completionItem.snippetSupport = true
-        lspconfig.cssls.setup(coq.lsp_ensure_capabilities({
-          capabilities = capabilities,
-          cmd = { "vscode-css-language-server", "--stdio" },
-          filetypes = { "css", "scss", "less" },
-        }))
-      end
-    }
+    {'neovim/nvim-lspconfig'}
   }
   -- Color scheme
   use 'srcery-colors/srcery-vim'
@@ -136,6 +101,45 @@ local plugins = require('packer').startup(function(use)
     require('packer').sync()
   end
 end)
+
+-- Configure LSP servers
+require("nvim-lsp-installer").setup {
+  automatic_installation = true,
+}
+local lspconfig = require("lspconfig")
+local coq = require("coq")
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+-- Here go the server setups
+lspconfig.sumneko_lua.setup(coq.lsp_ensure_capabilities({
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' }
+      }
+    }
+  }
+}))
+local eslint_config = require("lspconfig.server_configurations.eslint")
+lspconfig.eslint.setup(coq.lsp_ensure_capabilities{
+  cmd = { "yarn", "exec", unpack(eslint_config.default_config.cmd) }
+})
+lspconfig.volar.setup(coq.lsp_ensure_capabilities{
+  filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json'}
+})
+lspconfig.ember.setup(coq.lsp_ensure_capabilities{})
+lspconfig.cssls.setup(coq.lsp_ensure_capabilities{
+  capabilities = capabilities,
+  cmd = { "vscode-css-language-server", "--stdio" },
+  filetypes = { "css", "scss", "less" },
+})
+lspconfig.html.setup(coq.lsp_ensure_capabilities{
+  capabilities = capabilities,
+})
+lspconfig.stylelint_lsp.setup(coq.lsp_ensure_capabilities{})
+lspconfig.jsonls.setup(coq.lsp_ensure_capabilities{
+  capabilities = capabilities,
+})
 
 -- Dashboard
 vim.cmd("let g:dashboard_default_executive='fzf'")
