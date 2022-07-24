5449cb924b0449c69ecd594597974872995608d2
Ángel Brasero Moreno <angel.brasero@gmail.com>
2022-07-24 17:59:55 +0200

    Added auto sessions to nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,6 +4,8 @@ require('commands')
 require('tabline')
 
 vim.cmd('set clipboard=unnamedplus')
-vim.cmd('colorscheme gruvbox')
+vim.cmd('colorscheme srcery')
 vim.cmd('set number')
 vim.cmd('set mouse=a')
+vim.o.sessionoptions='blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal'
+
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -25,7 +25,9 @@ local plugins = require('packer').startup(function(use)
   }
   -- Color scheme
   use 'srcery-colors/srcery-vim'
-  use { "ellisonleao/gruvbox.nvim" }
+  use 'ellisonleao/gruvbox.nvim'
+  -- Sessions
+  use 'rmagatti/auto-session'
   -- Filesystem tree
   --use 'preservim/nerdtree'
   use {
@@ -143,6 +145,45 @@ lspconfig.jsonls.setup(coq.lsp_ensure_capabilities{
 
 -- Dashboard
 vim.cmd("let g:dashboard_default_executive='fzf'")
+local dashboard = require('dashboard')
+dashboard.custom_center = {
+  {
+    icon = '  ',
+    desc = 'Restore last session                    ',
+    shortcut = 'SPC s l',
+    action ='RestoreSession'
+  },
+  {
+    icon = '  ',
+    desc = 'Recent sessions                         ',
+    action =  'Autosession search',
+    shortcut = 'SPC f h'
+  },
+  {
+    icon = '  ',
+    desc = 'Find File                               ',
+    action = 'Telescope find_files find_command=rg,--hidden,--files',
+    shortcut = 'SPC f f'
+  },
+  {
+    icon = '  ',
+    desc ='File Browser                            ',
+    action =  'Telescope file_browser',
+    shortcut = 'SPC f b'
+  },
+  {
+    icon = '  ',
+    desc = 'Find word                               ',
+    action = 'Telescope live_grep',
+    shortcut = 'SPC f w'
+  },
+}
+
+-- Session
+require('auto-session').setup {
+  auto_save_enabled = true,
+  auto_restore_enabled = false
+}
 
 -- Treesitter
 require'nvim-treesitter.configs'.setup {
