c83bbfd64032fa5cad5183a453fbc5c21b907298
Ángel Brasero Moreno <angel.brasero@gmail.com>
2022-10-06 12:06:01 +0200

    Using gitsigns instead of gitgutter in nvim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -110,7 +110,14 @@ local plugins = require('packer').startup(function(use)
   use 'tpope/vim-fugitive'
   -- Suggestions and completion
   -- Git marks in the gutter
-  use 'airblade/vim-gitgutter'
+  --use 'airblade/vim-gitgutter'
+  use {
+    'lewis6991/gitsigns.nvim',
+    tag = 'release', -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
+    config = function()
+      require('gitsigns').setup()
+    end
+  }
   -- Git diff
   use {
     'sindrets/diffview.nvim',
