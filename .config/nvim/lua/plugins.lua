015116ac1ae4ed27c3171c25b46095d40a0d4e6f
Ángel Brasero Moreno <angel.brasero@gmail.com>
2022-10-20 15:23:34 +0200

    Using null-ls for formatting

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -53,6 +53,39 @@ local plugins = require('packer').startup(function(use)
     },
     'neovim/nvim-lspconfig'
   }
+  -- Null LSP
+  use {
+    'jose-elias-alvarez/null-ls.nvim',
+    requires = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local nullls = require('null-ls')
+      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+      nullls.setup{
+        on_attach = function(client, bufnr)
+          if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                    vim.lsp.buf.format({
+                      bufnr = bufnr,
+                      filter = function(cli)
+                        return cli.name == "null-ls"
+                      end
+                    })
+                end,
+            })
+          end
+        end,
+        sources = {
+          nullls.builtins.code_actions.eslint_d,
+          nullls.builtins.formatting.eslint_d,
+        }
+      }
+    end
+  }
   -- Completion
   use {
     'ms-jpq/coq_nvim',
