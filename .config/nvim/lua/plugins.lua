985c2578d606c37056ec35798fca1b68ca29acbd
Ángel Brasero Moreno <angel.brasero@gmail.com>
2023-06-15 15:15:41 +0200

    [Neovim] Adding the ability to close buffers from telescope buffer picker

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -308,6 +308,7 @@ require('lazy').setup({
       },
     },
     config = function()
+      local action_state = require 'telescope.actions.state'
       local telescope = require'telescope'
       telescope.setup{
         defaults = {
@@ -316,6 +317,43 @@ require('lazy').setup({
             limit_entries = 50,
           }
         },
+        pickers = {
+          buffers = {
+            mappings = {
+              i = {
+                ["<C-x>"] = function(prompt_bufnr)
+                local current_picker = action_state.get_current_picker(prompt_bufnr)
+                local selected_bufnr = action_state.get_selected_entry().bufnr
+                --- get buffers with lower number, with next highest ("bprevious") selected first
+                local replacement_buffers = {}
+                for entry in current_picker.manager:iter() do
+                  if entry.bufnr < selected_bufnr then
+                    table.insert(replacement_buffers, 1, entry.bufnr)
+                  end
+                end
+                current_picker:delete_selection(function(selection)
+                  local bufnr = selection.bufnr
+                  -- get associated window(s)
+                  local winids = vim.fn.win_findbuf(bufnr)
+                  -- verify to only close windows with current buffer
+                  local tabwins = vim.api.nvim_tabpage_list_wins(0)
+                  -- fill winids with new empty buffers
+                  for _, winid in ipairs(winids) do
+                    if vim.tbl_contains(tabwins, winid) then
+                      local new_buf = vim.F.if_nil(table.remove(replacement_buffers),
+                      -- if no alternative available, create unlisted scratch buffer
+                      vim.api.nvim_create_buf(false, true))
+                      vim.api.nvim_win_set_buf(winid, new_buf)
+                    end
+                  end
+                  -- remove buffer at last
+                  vim.api.nvim_buf_delete(bufnr, {force = true})
+                  end)
+                end
+              }
+            }
+          },
+        }
       }
       telescope.load_extension('fzf')
     end
@@ -329,7 +367,7 @@ require('lazy').setup({
   -- Underlines the current word and its appearances
   { 'itchyny/vim-cursorword' },
   -- Change easily the surrounding characters
-  -- { 'tpope/vim-surround' },
+  { 'tpope/vim-surround' },
   -- Autoclosing brackets
   { 'jiangmiao/auto-pairs' },
   -- For writers
