3c54e9d7fc64cb49e206addbc2f986198b5a1968
Ángel Brasero Moreno <angel.brasero@gmail.com>
2023-11-30 10:14:53 +0100

    [Scripts] Installing AGS and the screenshot tools

--- .scripts/install/installwayland.sh
+++ .scripts/install/installwayland.sh
@@ -6,10 +6,14 @@ yay -S \
 	nvidia-dkms \
 	wayland \
 	waylock swayidle waybar wlogout \
-	hyprland-nvidia hyprpaper \
+	hyprland-nvidia \
+	swww \
+	aylurs-gtk-shell \
 	xdg-desktop-portal-hyprland \
 	xorg-xhost \
 	wlr-randr \
 	wdisplays \
 	wl-clipboard \
+	grim slurp swappy \
+	#hyprpaper \
 	#wayfire wayfire-plugins-extra wcm wf-config wf-shell \
