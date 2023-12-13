c681c6dbc90c7c6413487f91373e3a7287a0bc1d
Ángel Brasero Moreno <angel.brasero@gmail.com>
2023-12-13 15:50:32 +0100

    [Rofi] Removed unused launchers

--- .config/rofi/launch_power.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-COLOR=white
-echo -e "shutdown\0icon\x1f<span color='${COLOR}'></span>\nreboot\0icon\x1f<span color='${COLOR}'></span>\nlock\0icon\x1f<span color='${COLOR}'></span>\nsuspend\0icon\x1f<span color='${COLOR}'></span>\nclose session\0icon\x1f<span color='${COLOR}'></span>" | rofi -config power.rasi -dmenu
