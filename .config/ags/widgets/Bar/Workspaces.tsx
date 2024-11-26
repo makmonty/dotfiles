1c3e8084a16a577b2cb0f0e4f5d2af292759dfd4
Ángel Brasero Moreno <angel.brasero@atani.com>
2024-11-26 13:38:14 +0100

    [AGS] Fixed ordering of workspaces in top bar

--- .config/ags/widgets/Bar/Workspaces.tsx
+++ .config/ags/widgets/Bar/Workspaces.tsx
@@ -1,5 +1,5 @@
-import { Gdk, Gtk } from "astal/gtk3"
-import Hyprland from "gi://AstalHyprland";
+import { Gdk, Gtk } from 'astal/gtk3'
+import Hyprland from 'gi://AstalHyprland';
 import GObject from 'astal/gobject'
 import { execAsync } from 'astal'
 
@@ -7,14 +7,33 @@ const hyprland = Hyprland.get_default()
 
 const monitors: Record<string, Record<string, { widget: GObject.Object, ws: Hyprland.Workspace }>> = {};
 
-const addWorkspace = (ws: Hyprland.Workspace, parent: any) => {
+const workspacesById: Record<string, string[]> = {}
+
+const addWorkspace = (ws: Hyprland.Workspace, parent: GObject.Object) => {
   const widget = Workspace({ ws });
-  parent.add(widget);
-  parent.reorder_child(widget, ws.id - 1)
-  parent.notify('children');
-  parent.show_all();
   monitors[ws.monitor.id] ||= {};
   monitors[ws.monitor.id][ws.id] = {widget, ws};
+  const position = findWorkspacePosition(ws);
+  workspacesById[ws.monitor.id] ||= [];
+  workspacesById[ws.monitor.id].splice(position, 0, ws.id)
+
+  parent.add(widget);
+  parent.reorder_child(widget, position)
+  parent.notify('children');
+  parent.show_all();
+}
+
+const findWorkspacePosition = (ws: Hyprland.Workspace) => {
+  const monitorId = ws.monitor.id;
+  let pos = 0;
+  for (const i in workspacesById[monitorId]) {
+    const currentWsId = workspacesById[monitorId][i]
+    const currentWs = monitors[monitorId][currentWsId]
+    if (ws.id > currentWs?.ws.id) {
+      pos = parseInt(i) + 1
+    }
+  }
+  return pos;
 }
 
 const removeWorkspace = (ws: Hyprland.Workspace, parent: any) => {
@@ -23,6 +42,8 @@ const removeWorkspace = (ws: Hyprland.Workspace, parent: any) => {
   parent.notify('children');
   parent.show_all();
   delete monitors[ws.monitor.id][ws.id];
+  const index = workspacesById[ws.monitor.id].findIndex(wsId => wsId === ws.id)
+  workspacesById[ws.monitor.id].splice(index, 1)
 }
 
 const removeEmptyWorkspaces = (parent: any, monitor: Hyprland.Monitor) => {
@@ -49,10 +70,7 @@ export function Workspace({ ws }: { ws: Hyprland.Workspace }) {
       self => {
         self.toggleClassName('special', ws.name === 'special');
         self.toggleClassName('focused', hyprland.get_focused_workspace().id === ws.id);
-        // TODO: ws.get_monitor().get_active_workspace() is returning always the same workspace
-        // Use the second implementation when it's fixed
-        self.toggleClassName('active', hyprland.get_focused_workspace().id === ws.id);
-        // self.toggleClassName('active', ws.get_monitor().get_active_workspace().id === ws.id);
+        self.toggleClassName('active', ws.get_monitor().get_active_workspace().id === ws.id);
       },
     )}
   >
