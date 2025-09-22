d30ea4a72a07fe2e6f26b0e48e59d809bf16c3a3
Ángel Brasero <372376+makmonty@users.noreply.github.com>
2025-09-22 10:59:42 +0200

    [Neovim] Enabling mini-surround and neo-tree

---
+++ .config/nvim/lua/plugins/neotree.lua
@@ -0,0 +1,15 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      bind_to_cwd = false,
+      follow_current_file = { enabled = true },
+      use_libuv_file_watcher = true,
+      filtered_items = {
+        visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+        hide_dotfiles = false,
+        hide_gitignored = true,
+      },
+    },
+  },
+}
