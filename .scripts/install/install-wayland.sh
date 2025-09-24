c8f13fb3be603cbe544c577319e9d3e820cff942
Ángel Brasero Moreno <makmonty@users.noreply.github.com>
2025-09-24 10:23:55 +0200

    [Scripts] Install git version of AGS

--- .scripts/install/install-wayland.sh
+++ .scripts/install/install-wayland.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
 yay -S \
-	wayland \
-	hyprland \
-	hyprlock hypridle wlogout \
-	swww \
-	aylurs-gtk-shell \
-	xdg-desktop-portal-hyprland \
-	xorg-xhost \
-	wlr-randr \
-	wdisplays \
-	wl-clipboard \
-	grim slurp swappy \
-	nwg-look \
-	emote \
-	brightnessctl \
-	rofi-wayland \
-	#hyprpaper \
-	#wayfire wayfire-plugins-extra wcm wf-config wf-shell \
+  wayland \
+  hyprland \
+  hyprlock hypridle wlogout \
+  swww \
+  aylurs-gtk-shell-git \
+  xdg-desktop-portal-hyprland \
+  xorg-xhost \
+  wlr-randr \
+  wdisplays \
+  wl-clipboard \
+  grim slurp swappy \
+  nwg-look \
+  emote \
+  brightnessctl \
+  rofi-wayland
+#hyprpaper \
+#wayfire wayfire-plugins-extra wcm wf-config wf-shell \
