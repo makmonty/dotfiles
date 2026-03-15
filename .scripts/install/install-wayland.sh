608cd58bba81365531fab36ba5ec1fde53533e32
Ángel Brasero <372376+makmonty@users.noreply.github.com>
2026-03-15 22:48:06 +0100

    Switching from deprecated swww to awww

--- .scripts/install/install-wayland.sh
+++ .scripts/install/install-wayland.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
 yay -S \
-  wayland \
-  hyprland \
-  hyprlock hypridle wlogout \
-  swww \
-  xdg-desktop-portal-hyprland \
-  xorg-xhost \
-  wlr-randr \
-  wdisplays \
-  waybar \
-  wlogout \
-  dunst \
-  swayosd \
-  wl-clipboard \
-  grim slurp swappy \
-  nwg-look \
-  emote \
-  brightnessctl \
-  vicinae
+	wayland \
+	hyprland \
+	hyprlock hypridle wlogout \
+	awww-git \
+	xdg-desktop-portal-hyprland \
+	xorg-xhost \
+	wlr-randr \
+	wdisplays \
+	waybar \
+	wlogout \
+	dunst \
+	swayosd \
+	wl-clipboard \
+	grim slurp swappy \
+	nwg-look \
+	emote \
+	brightnessctl \
+	vicinae
 #rofi-wayland \
 #aylurs-gtk-shell-git \
 #hyprpaper \
