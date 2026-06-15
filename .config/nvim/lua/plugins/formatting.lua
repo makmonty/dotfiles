0d61f9ddde54e869324b0a45c1f95405efa773a2
Ángel Brasero <372376+makmonty@users.noreply.github.com>
2026-06-15 10:33:37 +0200

    [Neovim] Reactivating Conform to format

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -1,29 +1,33 @@
 return {
-  -- {
-  --   "stevearc/conform.nvim",
-  --   -- Everything in opts will be passed to setup()
-  --   opts = {
-  --     -- Define your formatters
-  --     formatters_by_ft = {
-  --       lua = { "stylua" },
-  --       javascript = { "eslint", "prettierd", "prettier", stop_after_first = true },
-  --       typescript = { "eslint", "prettierd", "prettier", stop_after_first = true },
-  --     },
-  --     -- Set up format-on-save
-  --     format_on_save = { timeout_ms = 100, lsp_format = "fallback" },
-  --     -- Customize formatters
-  --     -- formatters = {
-  --     --   shfmt = {
-  --     --     prepend_args = { "-i", "2" },
-  --     --   },
-  --     -- },
-  --   },
-  --   -- log_level = vim.log.levels.TRACE,
-  --   -- init = function()
-  --   --   -- If you want the formatexpr, here is the place to set it
-  --   --   vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-  --   -- end,
-  -- },
+  {
+    "stevearc/conform.nvim",
+    -- Everything in opts will be passed to setup()
+    opts = {
+      -- Define your formatters
+      formatters_by_ft = {
+        lua = { "stylua" },
+        javascript = { "prettierd", "prettier", "eslint_d" },
+        typescript = { "prettierd", "prettier", "eslint_d" },
+        vue = { "prettierd", "prettier", "eslint_d" },
+      },
+      -- Set up format-on-save
+      format_on_save = {
+        -- timeout_ms = 100,
+        lsp_format = "fallback",
+      },
+      -- Customize formatters
+      -- formatters = {
+      --   shfmt = {
+      --     prepend_args = { "-i", "2" },
+      --   },
+      -- },
+    },
+    -- log_level = vim.log.levels.TRACE,
+    -- init = function()
+    --   -- If you want the formatexpr, here is the place to set it
+    --   vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    -- end,
+  },
   -- {
   --   "mhartington/formatter.nvim",
   --
